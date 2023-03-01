68e1395f27f2e690137ee424d2ff1860fc072c31
godfreykisero <godfreyykkisero@gmail.com>
2023-03-01 08:01:50 -0800

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@
  * Return: a pointer to the resulting string dest.
  */
 
-char _strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 
 {
 	int index = 0, dest_len = 0;
