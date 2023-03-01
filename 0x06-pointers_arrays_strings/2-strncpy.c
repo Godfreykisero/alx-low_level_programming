16f9d67e9cb40cca3a3790845fcb03a048613951
godfreykisero <godfreyykkisero@gmail.com>
2023-03-01 09:32:03 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,12 @@
 
 /**
  * _strncpy - copy a string
- * @dset: input value
+ * @dest: input value
  * @src: input value
  * @n: input value
  * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
-
 {
 	int j;
 
