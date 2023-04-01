067312791e6f2aa61ee9c04f8541f1718343e2cf
godfreykisero <godfreyykkisero@gmail.com>
2023-04-01 12:14:59 -0700

    conflicting

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
-* list_len - returns the number of elements in a linked list
-* @h: pointer to the list_t list
-*
-* Return: number of elements in h
-*/
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
 ize_t list_len(const list_t *h)
 
 {
